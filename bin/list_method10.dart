3a6d76ed35b53705b164e1bb507341f5f8f1e4d5
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-18 23:19:23 +0500

    10

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -5,6 +5,28 @@
         list1(list): parameter
     Returns:
         list: return answer
-*/
+*/List func(List x){
+    int i=0;
+    num a=0;
+    num y=0;
+    List n=[]; 
+    int l=x.length;
+    while(l>i){
+      if(x[i]==0){
+        a+=1;
+      }else{
+       y+=1;
+        }
+        i+=1;
+    }
+    a.toString();
+    y.toString();
+    n.add(y);
+    n.add(a);
+    return n; 
+}
 
-void main() {}
+void main(){   
+print(func([0,1,0,0,0,1,0]));
+}
+
